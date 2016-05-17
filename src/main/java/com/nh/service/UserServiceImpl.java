116f1f5bc88298177ca8034722e9ef0c9fc8176b
HelderVSousa <helder.v.sousa@itgrow.pt>
2016-05-17 15:31:08 +0100

    commit CHECK

--- src/main/java/com/nh/service/UserServiceImpl.java
+++ src/main/java/com/nh/service/UserServiceImpl.java
@@ -10,12 +10,14 @@ import com.nh.model.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author hvsousa
  */
 @Service("userService")
+@Transactional
 public class UserServiceImpl  implements  UserService{
     
     @Autowired
