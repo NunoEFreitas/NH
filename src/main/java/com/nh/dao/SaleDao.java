39704fb47245bc7f190ad295abcc07efb5b2b9eb
HelderVSousa <helder.v.sousa@itgrow.pt>
2016-05-19 13:53:50 +0100

    working

--- src/main/java/com/nh/dao/SaleDao.java
+++ src/main/java/com/nh/dao/SaleDao.java
@@ -1,23 +1,23 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.nh.dao;
-//
-//import com.nh.model.Sale;
-//import java.util.List;
-//
-///**
-// *
-// * @author hvsousa
-// */
-//public interface SaleDao {
-//    
-//    void addSale(Sale sale);
-//    Sale findSaleById(int id);
-//    List<Sale> getAllSales();
-//    void updateSale(Sale sale);
-//    void deleteSale(int id);
-//    
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nh.dao;
+
+import com.nh.model.Sale;
+import java.util.List;
+
+/**
+ *
+ * @author hvsousa
+ */
+public interface SaleDao {
+    
+    void addSale(Sale sale);
+    Sale findSaleById(int id);
+    List<Sale> getAllSales();
+    void updateSale(Sale sale);
+    void deleteSale(int id);
+    
+}
--- src/main/java/com/nh/service/SaleService.java
+++ src/main/java/com/nh/service/SaleService.java
@@ -1,21 +1,21 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.nh.service;
-//
-//import com.nh.model.Sale;
-//import java.util.List;
-//
-///**
-// *
-// * @author hvsousa
-// */
-//public interface SaleService {
-//    void addSale(Sale sale);
-//    Sale findSaleById(int id);
-//    List<Sale> getAllSales();
-//    void updateSale(Sale sale);
-//    void deleteSale(int id);
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nh.service;
+
+import com.nh.model.Sale;
+import java.util.List;
+
+/**
+ *
+ * @author hvsousa
+ */
+public interface SaleService {
+    void addSale(Sale sale);
+    Sale findSaleById(int id);
+    List<Sale> getAllSales();
+    void updateSale(Sale sale);
+    void deleteSale(int id);
+}
